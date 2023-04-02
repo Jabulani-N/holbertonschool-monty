ef492f560cf28e713f8f4028fbec7905960836a8
Jabulani-N <jabu.94@gmail.com>
2023-04-02 15:24:01 -0700

    I_can_now_read_files

--- monty.h
+++ monty.h
@@ -1,3 +1,15 @@
+
+
+
+#ifndef monty_H
+#define monety_h
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+
+
 /**
  * struct stack_s - doubly linked list representation of a stack (or queue)
  * @n: integer
@@ -27,4 +39,14 @@ typedef struct instruction_s
   char *opcode;
   void (*f)(stack_t **stack, unsigned int line_number);
 } instruction_t;
+
+typedef struct global_s
+{
+	char *fileName;
+} global_t;
+/*declare an external (global) glob_s structure*/
+extern global_t glob;
+
+
+#endif
 
